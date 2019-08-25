17bf31bfef123cd2f1aebd7b4ee9291e91d0ae12
git-yulia <julia.maliauka953@myci.csuci.edu>
2019-08-25 14:54:42 -0700

    Added PulseWaveform class to header

--- intro_to_radar/intro_to_radar/header.h
+++ intro_to_radar/intro_to_radar/header.h
@@ -1,9 +1,28 @@
+#pragma once 
+
 #ifndef HEADER_H
 #define HEADER_H
 
 typedef double KILOMETERS; 
-typedef double MICROSECONDS; 
+typedef double MICROSECONDS;
+typedef int KILOWATTS; 
+typedef int HERTZ; 
+typedef int JOULES; 
 
 #define c 300000 // km per second 
+
+class PulseWaveform
+{
+public:
+	PulseWaveform(); 
+
+private:
+	KILOWATTS peak_power;
+	KILOWATTS average_power; 
+	HERTZ pulse_repetition_frequency;
+	MICROSECONDS pulse_repetition_period; 
+	MICROSECONDS pulse_width; 
+	JOULES pulse_energy; 
+};
 
 #endif
